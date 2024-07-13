a06a81f06b7f458d61b86e58e206e3c443b39788
gato365 <immanuelwilliams@gato365.com>
2024-07-12 19:38:24 -0700

    Made the Posts show on home page using for loop

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+
+## Fake Posts
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'The First Post',
+        'content': 'This is the content of the first post.',
+        'date_posted': '2023-04-01'
+    },
+    {
+        'author': 'Jane Smith',
+        'title': 'Exploring Python',
+        'content': 'Python has numerous applications, from web development to data science.',
+        'date_posted': '2023-04-02'
+    },
+    {
+        'author': 'Mike Johnson',
+        'title': 'Django for Beginners',
+        'content': 'Django makes it easier to build Web applications quickly and with less code.',
+        'date_posted': '2023-04-03'
+    },
+    {
+        'author': 'Emily Davis',
+        'title': 'Understanding REST APIs',
+        'content': 'REST APIs allow different systems to communicate with each other over HTTP.',
+        'date_posted': '2023-04-04'
+    }
+]
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts 
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
