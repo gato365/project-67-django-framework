a984e98a551ba1f6bad087da0aff16f9a22e3b72
gato365 <immanuelwilliams@gato365.com>
2024-07-12 22:07:20 -0700

    Made Posts rather fake post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,41 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 
 
-## Fake Posts
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'The First Post',
-        'content': 'This is the content of the first post.',
-        'date_posted': '2023-04-01'
-    },
-    {
-        'author': 'Jane Smith',
-        'title': 'Exploring Python',
-        'content': 'Python has numerous applications, from web development to data science.',
-        'date_posted': '2023-04-02'
-    },
-    {
-        'author': 'Mike Johnson',
-        'title': 'Django for Beginners',
-        'content': 'Django makes it easier to build Web applications quickly and with less code.',
-        'date_posted': '2023-04-03'
-    },
-    {
-        'author': 'Emily Davis',
-        'title': 'Understanding REST APIs',
-        'content': 'REST APIs allow different systems to communicate with each other over HTTP.',
-        'date_posted': '2023-04-04'
-    }
-]
 
 # Create your views here.
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'HOME'
     }
     return render(request, 'blog/home.html', context)
