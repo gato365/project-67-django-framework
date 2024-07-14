0309cf248ede54063405b87502c64e1a4b9244c8
gato365 <immanuelwilliams@gato365.com>
2024-07-13 18:17:01 -0700

    Made profile but it does not work entirely well.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -132,3 +132,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html', next_page='logged_out'), name='logout'),
     path('', include('blog.urls')),
-    
+ path('profile/', user_views.profile, name='profile'),   
 ]
