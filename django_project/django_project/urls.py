63c75f0a498f746bbb2e41229305077b1a734947
gato365 <immanuelwilliams@gato365.com>
2024-07-14 04:49:40 -0700

    Fixed Log ut issue and added name at top f page when lgged in

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,7 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html', next_page='logged_out'), name='logout'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='logout.html', next_page='logged_out'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', include('blog.urls')),
  path('profile/', user_views.profile, name='profile'),   
 ]
